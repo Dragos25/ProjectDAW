f4cd8f4f34d9a60608149eacedcdd82b67b98fdf
Dragos Iasageanu <iasageanu.dragos@gmail.com>
2022-01-23 20:58:34 +0200

    created basic project

---
+++ DAW Project/Context/IApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DAW_Project.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAW_Project.Context
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<User> Users { get; set; }
+
+        Task<int> SaveChanges();
+    }
+}
---
+++ DAW Project/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DAW_Project.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public String username { get; set; }
+        public String password { get; set; }
+        public String firstName { get; set; }
+        public String lastName { get; set; }
+
+    }
+}
